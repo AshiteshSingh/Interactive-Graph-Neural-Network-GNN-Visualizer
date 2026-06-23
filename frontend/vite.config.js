2279a01f3960d22834b296dbcedd7657b8aeb21f
AshiteshSingh <raghuvesh1285@gmail.com>
2026-06-23 19:12:30 +0530

    Fix-Vercel-Base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Interactive-Graph-Neural-Network-GNN-Visualizer/',
+  plugins: [react()]
 })
